3201a695ef8002014919b2950cb83d19d457a688
SanaiD <99264174+SanaiDelapara@users.noreply.github.com>
2022-02-15 13:56:38 +0000

    Line 21   while length < 16 and length > 8 :

--- make_password.py
+++ make_password.py
@@ -6,19 +6,19 @@ def make_password():
   #Make a list of characters to use for passwords
   characters = string.ascii_letters + string.digits + string.punctuation
 
-  #Get info from the user
+  #Get info from the user 
   site_app = input("What site or app are you making a password for?")
   os.system("clear")
 
   user_name = input("What is your username for the site or app?")
   os.system("clear")
 
-  length = int(input("How long do you want your password?"))
+  length = int(input("How long do you want your password? max character 16 and minimum 8"))
   os.system("clear")
 
   #Generate the password 
   password = ""
-  while length < 16:
+  while length < 16 and length > 8 :
     password = password + random.choice(characters)
     length -=1
 
