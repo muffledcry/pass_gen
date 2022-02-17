43a05552335528f1be16325021793e53676ea059
Guychara Lafontant <99663161+Gucci36446@users.noreply.github.com>
2022-02-17 13:45:39 +0000

    We added the "change password" code. It prints out the data in file.

--- showpasswords.py
+++ showpasswords.py
@@ -4,27 +4,14 @@ import os
 def show_password():
   with open("passwords.txt") as fobj:
      lines = fobj.readlines()
-  # print(lines)
-  sanitized = []
+  count = 0
   for line in lines:
-    if line == '\n':
-      continue
-    else:
-      line = line.strip()
-      # sanitized.append(line)
-      for i in range(0, len(line), 3):
-        print(line)
+    count+=1
+    line = line.strip()
+    print(line)
 
-
-  password_dict = {sanitized[i]:{"username":sanitized[i + 1], "password": sanitized[i + 2]} for i in range(0, len(sanitized), 3)}
-
-  #for values in :
-  # print(password_dict)
 """"
 - make it look neat
-
-
-
 
 """
   
